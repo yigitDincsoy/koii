a0aebcf92c39693650ae8c46df24f60b6ca99827
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-05-30 17:02:13 -0300

    added the server functionality

--- src/Main.js
+++ src/Main.js
@@ -3,8 +3,6 @@ import "./Main.css";
 const { encrypt, decrypt, nonce } = require("solana-encryption");
 const Keypair = require("@solana/web3.js").Keypair;
 
-
-
 function Main() {
   const [encryptedData, set_encryptedData] = useState("");
   const [decryptedData, set_decryptedData] = useState("");
@@ -36,8 +34,26 @@ function Main() {
 
       </div>
 
+      <div className = "box">
+        <h2>New functionality!</h2>
+        <h3>Try our API now:</h3>
+
+
+        <input type="text" id="userInputAPI"   placeholder="Enter message to encrypt"/>
+      <button
+        onClick={() => {
+          const inputValue = document.getElementById("userInputAPI").value;
+          postRequest(inputValue);
+        }}
+      > Send a POST Request</button>
+        <p id="serverResponseRenderArea"></p>
+
+        </div>
+
  
     </div>
+
+
 
 
   );
@@ -80,6 +96,40 @@ function Main() {
     set_decryptedData(decrypted);
   }
 
+
+  function postRequest(arg1) {
+// get the data 
+let data = {
+  string: arg1,
+};
+
+// Turn the data into JSON
+let jsonData = JSON.stringify(data);
+
+// Define the request details
+let url = 'http://localhost:3000/listener';
+let options = {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json'
+  },
+  body: jsonData
+};
+
+// Send the request
+fetch(url, options)
+  .then(response => response.json()) 
+  .then(responseData => {
+      // Do something with the response
+      console.log(responseData);
+
+      document.getElementById("serverResponseRenderArea").innerHTML =  JSON.stringify(responseData);
+
+
+  })
+  .catch(error => console.error('Error:', error));
+
+  }
 
   
 
